83ceccd46f629fb8d170b47074b021469da90d96
Kate <katy8kirichenko@gmail.com>
2021-01-28 17:05:34 +0300

    error card delete

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const form = document.querySelector(".form");
 const desk = document.querySelector(".cards");
+const deskProgress =  document.querySelector(".card_progress");
+const deskDone =  document.querySelector(".card_done");
+const deskDel =  document.querySelector(".card_del");
 const data = [];
 
 const submitBtm = document.querySelector(".btn_submit");
@@ -11,8 +14,8 @@ const showTask = () => {
 desk.innerHTML = "";
   data.forEach((item) => {
     desk.innerHTML += `<div class="card_text">
-    <h3>${item.note}</h3>
-    <p>${item.content}</p>
+    <h3 class="card_task">${item.note}</h3>
+    <p class="card_comment">${item.content}</p>
     <button class="btn_card" id="redact">&#128394;</button>
     <button class="btn_card" id ="delete">&#128465;</button>
     <button class="btn_card" id ="done">&#10003;</button>
@@ -23,8 +26,8 @@ desk.innerHTML = "";
 const searchTask = (event)=>{
   const card = event.target.closest(".card_text");
 
-  const cardTask = card.querySelector(".task").textContent;
-  const cardComment = card.querySelector(".comment").textContent;
+  const cardTask = card.querySelector(".card_task").textContent;
+  const cardComment = card.querySelector(".card_comment").textContent;
 
   data.forEach(function (el, ind) {
     if (el.note === cardTask && el.comment === cardComment) {
@@ -33,8 +36,12 @@ const searchTask = (event)=>{
   })
 }
 
-const delTask=(index)=>{
-  data.splice(index, 1);
+const delTask=(i)=>{
+  .innerHTML += `<div class="card_text">
+  <h3 class="card_task">${i.note}</h3>
+  <p class="card_comment">${i.content}</p>
+</div>`;
+  
   showTask();
 }
 
@@ -55,14 +62,14 @@ desk.addEventListener("click", (event)=>{
 
   if (event.target.closest("#redact")){
 
-    searchTask()
+    searchTask(event)
   } 
 if(event.target.closest("#delete")){
 
-  searchTask();
-  delTask(index);
+  searchTask(event);
+  delTask( index);
 }
 if(event.target.closest("#done")){
-  searchTask()
+  searchTask(event)
 }
 })
