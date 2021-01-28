6216bd862f3406ea5585be5ffca9792d7f27f86d
Kate <katy8kirichenko@gmail.com>
2021-01-28 13:04:24 +0300

    error delete

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const form = document.querySelector(".form");
+const desk = document.querySelector(".cards");
 const data = [];
-const card = document.querySelector(".cards");
+
 const submitBtm = document.querySelector(".btn_submit");
+const delBtn = document.querySelector("#delete");
+
+let index
 
 const showTask = () => {
-  card.innerHTML = "";
+desk.innerHTML = "";
   data.forEach((item) => {
-    card.innerHTML += `<div class="card_text">
+    desk.innerHTML += `<div class="card_text">
     <h3>${item.note}</h3>
     <p>${item.content}</p>
-    <button class="btn_card">&#128394;</button>
-    <button class="btn_card">&#128465;</button>
-    <button class="btn_card">&#10003;</button>
+    <button class="btn_card" id="redact">&#128394;</button>
+    <button class="btn_card" id ="delete">&#128465;</button>
+    <button class="btn_card" id ="done">&#10003;</button>
   </div>`;
   });
 };
 
-// const showCard = () => {
-//   card.style.display = "block";
-// };
+const searchTask = (event)=>{
+  const card = event.target.closest(".card_text");
+
+  const cardTask = card.querySelector(".task").textContent;
+  const cardComment = card.querySelector(".comment").textContent;
+
+  data.forEach(function (el, ind) {
+    if (el.note === cardTask && el.comment === cardComment) {
+      index = ind;
+    }
+  })
+}
+
+const delTask=(index)=>{
+  data.splice(index, 1);
+  showTask();
+}
 
 submitBtm.addEventListener("click", (event) => {
   event.preventDefault();
@@ -28,7 +46,23 @@ submitBtm.addEventListener("click", (event) => {
 
   data.push({ note: task.value, content: comment.value });
 
-  //   showCard();
   showTask();
   form.reset();
 });
+
+desk.addEventListener("click", (event)=>{
+  event.preventDefault();
+
+  if (event.target.closest("#redact")){
+
+    searchTask()
+  } 
+if(event.target.closest("#delete")){
+
+  searchTask();
+  delTask(index);
+}
+if(event.target.closest("#done")){
+  searchTask()
+}
+})
